3a6319dbd98d9236bf4e657c69e6300169a83aa3
Juan11211 <avilesjuan628@gmail.com>
2023-05-23 14:03:01 -0400

    Almost done

--- pages/components/Hero.js
+++ pages/components/Hero.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='flex items-center justify-center h-screen mb-12 bg-fixed'  >
-        <div />
+    <div className='flex items-center text-white justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'  >
+            {/* overlay */}
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]'/>
 
-        <div>
+        <div className='p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]'>
             <h2>Heading</h2>
                 <p>Message</p>
                 <button>Book</button>
