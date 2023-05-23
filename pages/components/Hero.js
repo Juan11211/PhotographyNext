1329dd247da38c1136a331da1324a7f54f348f5d
Juan11211 <avilesjuan628@gmail.com>
2023-05-22 22:58:48 -0400

    Working on nextjs app

---
+++ pages/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Hero() {
+  return (
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed'  >
+        <div />
+
+        <div>
+            <h2>Heading</h2>
+                <p>Message</p>
+                <button>Book</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
