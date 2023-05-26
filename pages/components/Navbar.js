4ec2f84c6452166ef67a53e3c9fd6fb71156b42b
Juan11211 <avilesjuan628@gmail.com>
2023-05-26 17:56:41 -0400

    Done

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -34,13 +34,13 @@ function Navbar() {
         </div>
         <ul style={{ color: `${textColor}`}} className='hidden sm:flex'>
           <li className='p-4'>
-            <Link href='/home'>Home</Link>
+            <Link href='/'>Home</Link>
           </li>
           <li className='p-4'>
             <Link href='/#gallery'>Gallery</Link>
           </li>
           <li className='p-4'>
-            <Link href='/portfolio'>Work</Link>
+            <Link href='/work'>Work</Link>
           </li>
           <li className='p-4'>
             <Link href='/contact'>Contact</Link>
@@ -62,16 +62,16 @@ function Navbar() {
         }
       >
         <ul>
-          <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+          <li onClick={handleNav} className='p-4 text-3xl  text-white hover:text-gray-500'>
             <Link href='/'>Home</Link>
           </li>
-          <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+          <li onClick={handleNav} className='p-4 text-3xl  text-white hover:text-gray-500'>
             <Link href='/#gallery'>Gallery</Link>
           </li>
-          <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+          <li onClick={handleNav} className='p-4 text-3xl text-white hover:text-gray-500'>
             <Link href='/work'>Work</Link>
           </li>
-          <li onClick={handleNav} className='p-4 text-3xl hover:text-gray-500'>
+          <li onClick={handleNav} className='p-4 text-3xl text-white hover:text-gray-500'>
             <Link href='/contact'>Contact</Link>
           </li>
         </ul>
--- pages/work.js
+++ pages/work.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Portfolio from './components/Portfolio'
 
-function Work() {
+function work() {
   return (
     <div>
-      
+      <Hero heading='Check Out' message='Some of my recent work' />
+      <Portfolio />
     </div>
   )
 }
 
-export default Work
+export default work
